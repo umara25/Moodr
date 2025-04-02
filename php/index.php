987d383cd0b7dfbc95ccc5a7984652035a121604
Umar <umarahmer420@gmail.com>
2025-04-02 18:45:53 -0400

    commit

--- php/index.php
+++ php/index.php
@@ -26,21 +26,21 @@ This is the splash page which users will first be greeted with. It holds announc
     <div id="container">
         <div id="header">
 
-            <p class="nav">M o o d r 
-            <?php 
-            if($loggedIn){
-                if ($_SESSION["role"] === "admin") {
-                    echo " A d m i n";
-                } 
-            }
-            ?>
+            <p class="nav">M o o d r
+                <?php
+                if ($loggedIn) {
+                    if ($_SESSION["role"] === "admin") {
+                        echo " A d m i n";
+                    }
+                }
+                ?>
             </p>
             <div class="nav-links">
                 <button id="dashb-btn" class="nav">Dashboard</button>
                 <button id="cal-btn" class="nav">Calendar</button>
                 <button id="review-btn" class="nav">Reviews</button>
                 <?php // If admin, they will have a user management button.
-                if($loggedIn){
+                if ($loggedIn) {
                     if ($_SESSION["role"] === "admin") {
                         echo "<button id='usermang-btn' class='nav'>User Managment</button>";
                     }
@@ -48,35 +48,37 @@ This is the splash page which users will first be greeted with. It holds announc
                 ?>
                 <button id="myprofile-btn" class="nav">My Profile</button>
                 <?php
-                if(!isset($_SESSION["username"])){
+                if (!isset($_SESSION["username"])) {
                     echo "<button id='loginpage-btn' class='nav'>Log in</button>";
-                }else{
+                } else {
                     echo "<button id='loginpage-btn' class='nav'>Log out</button>";
                 }
                 ?>
-                
+
             </div>
 
         </div>
         <div id="content">
             <div id="user-intro">
                 <?php
-                if(isset($_SESSION['username'])){
+                if (isset($_SESSION['username'])) {
                 ?>
                     <h1>Welcome <?= $_SESSION['username'] ?>!</h1>
                     <p>What's on the agenda for today?</p>
                 <?php
-                }else{
+                } else {
                     echo "<h1>Welcome to Mood FM!</h1>";
                     echo "<p>What's on the agenda for today?</p>";
                 }
                 ?>
-                
-                
+
+
             </div>
-            <div id = announcments>
-                <h2>Latest Announcement</h2>
-                <li>Mood FM Exec Meeting – April 4th @ 6 PM</li>
+            <div id="announcments">
+                <h1>Latest Announcements</h1>
+                <ul>
+                    <li>Mood FM Exec Meeting – April 4th @ 6 PM</li>
+                </ul>
             </div>
         </div>
         <div id="footer"> </div>
