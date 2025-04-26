4708a0010c6f872b572db5798f82c35b587d029b
needlesndpins <cinnamondream79@gmail.com>
2025-04-26 19:41:24 -0400

    Added about section css

--- js/indexStyleAdmin.js
+++ js/indexStyleAdmin.js
@@ -11,6 +11,7 @@ window.addEventListener("load", function (event) {
         let textbox1 = document.getElementById("user-intro");
         let textbox2 = document.getElementById("make-post");
         let textbox3 = document.getElementById("announcments");
+        let textbox4 = document.getElementById("about-us");
         let headers = document.getElementsByTagName("h1");
         let pars = document.getElementsByTagName("p");
         let labels = document.getElementsByTagName("label");
@@ -20,6 +21,7 @@ window.addEventListener("load", function (event) {
         textbox1.style["background-color"] = styleArr["textbox"];
         textbox2.style["background-color"] = styleArr["textbox"];
         textbox3.style["background-color"] = styleArr["textbox"];
+        textbox4.style["background-color"] = styleArr["textbox"];
         for(let i = 0; i < headers.length; i++) {
             headers[i].style.color = styleArr["text"];
         }
