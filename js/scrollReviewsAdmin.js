fd56af0b2f34d091818d8e0211b174e675dd706a
needlesndpins <cinnamondream79@gmail.com>
2025-04-25 21:54:12 -0400

    Made new reviews update css and added css

--- js/scrollReviewsAdmin.js
+++ js/scrollReviewsAdmin.js
@@ -13,6 +13,7 @@ window.addEventListener("load", function (event) {
 
     // Add scroll event listener
     window.addEventListener("scroll", send_requests);
+    window.addEventListener("scroll", updateCSS);
 
     /**
      * Sends AJAX requests once end of screen
@@ -248,6 +249,35 @@ window.addEventListener("load", function (event) {
             }
         }
     }
+
+    function updateCSS() {
+        fetch("style.php")
+        .then(response => response.json())
+        .then(success)
+        .catch(error => console.error("Fetch error:", error));
+
+        function success(styleArr) {
+            console.log("Got Here");
+            let textbox1 = document.querySelectorAll(".review-content");
+            let textbox3 = document.querySelectorAll(".triangle");
+            let headers = document.getElementsByTagName("h1");
+            let pars = document.getElementsByTagName("p");
+
+            textbox1.forEach(elm => {
+                elm.style["background-color"] = styleArr["textbox"];
+            });
+            textbox3.forEach(elm => {
+                elm.style.borderRight = "20px solid " + styleArr["textbox"];
+            });
+            for(let i = 0; i < headers.length; i++) {
+                headers[i].style.color = styleArr["text"];
+            }
+            for(let i = 1; i < pars.length; i++) {
+                pars[i].style.color = styleArr["text"];
+            }
+        }
+    }
+
 
 });
 
