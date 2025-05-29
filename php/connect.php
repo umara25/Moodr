e10eef24c43e8d79803ac939f13d8c8f9fdc32ff
umara25 <umarahmer420@gmail.com>
2025-05-29 16:31:57 -0400

    got rid of comment

--- php/connect.php
+++ php/connect.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Include this to connect. Change the dbname to match your database,
- * and make sure your login information is correct after you upload 
- * to csunix or your app will stop working.
- * 
- * Sam Scott, McMaster University, 2025
- */
 try {
     $dbh = new PDO(
         "mysql:host=localhost;dbname=FinalProject",
